5ce9535946d06adf2a942cfca1f3fd0cde8036ad
Paul Annesley <paul@annesley.cc>
2008-12-23 17:40:07 +1100

    Major style overhaul - minimalist, white background. Includes addition of 'About' page.

--- blog/views.py
+++ blog/views.py
@@ -10,83 +10,9 @@ from calendar import timegm
 from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
-from django.utils.feedgenerator import Atom1Feed
 from pdawebsite.blog.models import *
-
-def feed(request):
-
-	# everyone but feedburner and feedvalidator should go via feedburner
-	if __redirectFeed(request):
-		return HttpResponseRedirect('http://feeds.feedburner.com/paulannesley')
-
-	feed = Atom1Feed(
-		title = u"paul.annesley.cc",
-		link = request.build_absolute_uri('/'),
-		description = u"Paul Annesley on all things web",
-		subtitle = u"Paul Annesley on all things web",
-		language = u"en",
-		author_name = u"Paul Annesley",
-		author_link = u"http://paul.annesley.cc/",
-		feed_url = request.build_absolute_uri()
-	)
-
-	posts = BlogPost.objects.all()
-
-	for post in posts:
-		feed.add_item(
-			unique_id = post.get_unique_id(),
-			link = request.build_absolute_uri(post.get_absolute_url()),
-			title = post.title,
-			description = post.content,
-			pubdate = post.timecreated,
-			# TODO: move these into the BlogPost model
-			author_name = u"Paul Annesley",
-			author_link = request.build_absolute_uri('/')
-		)
-
-	return HttpResponse(
-		feed.writeString('utf-8'),
-		'application/atom+xml; charset=utf-8'
-		# let FireFox view source properly during development
-		#'text/xml; charset=utf-8'
-	)
-
-
-def comment_feed(request):
-
-	feed = Atom1Feed(
-		title = u"paul.annesley.cc comments",
-		link = request.build_absolute_uri('/'),
-		description = u"All comments at paul.annesley.cc",
-		language = u"en",
-		author_name = u"Paul Annesley",
-		author_link = u"http://paul.annesley.cc/",
-		feed_url = request.build_absolute_uri()
-	)
-
-	comments = BlogPostComment.objects.filter(approved=1)
-
-	for comment in comments:
-		feed.add_item(
-			unique_id = comment.get_unique_id(),
-			link = request.build_absolute_uri(comment.get_absolute_url()),
-			title = comment,
-			description = comment.content,
-			pubdate = comment.timecreated,
-			author_name = comment.authorname,
-			author_link = comment.authorurl
-		)
-
-	return HttpResponse(
-		feed.writeString('utf-8'),
-		'application/atom+xml; charset=utf-8'
-		# let FireFox view source properly during development
-		#'text/xml; charset=utf-8'
-	)
-
 
 def article(request, slug):
 
@@ -165,6 +91,10 @@ def archive(request):
 	title = u'Archive'
 	return render_to_response('archive.html', locals())
 
+def about(request):
+	feeds = __getAllFeeds()
+	title = u'About Paul Annesley and This Site'
+	return render_to_response('about.html', locals())
 
 ###################
 # helper functions
@@ -245,12 +175,6 @@ def __getFeeds(feedinfo):
 			cache.set(cacheKey, feed, __randomizeCacheMinutes(cacheMinutes))
 
 	return feeds
-
-def __redirectFeed(request):
-	return not re.search(
-		r'(feedburner|feedvalidator)',
-		request.META['HTTP_USER_AGENT'], re.I
-	)
 
 def __isCommentSpam(comment):
 
--- urls.py
+++ urls.py
@@ -2,18 +2,19 @@ import os
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
-import pdawebsite.blog.views
+import pdawebsite.blog.views, pdawebsite.blog.feedviews
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	(r'^admin/(.*)', admin.site.root),
 	(r'^$', pdawebsite.blog.views.frontpage),
+	(r'^about$', pdawebsite.blog.views.about),
 	(r'^articles$', pdawebsite.blog.views.archive),
-	(r'^feed/?$', pdawebsite.blog.views.feed), # legacy wordpress url
-	(r'^feed/atom/?$', pdawebsite.blog.views.feed), # legacy wordpress url
-	(r'^articles/feed/?$', pdawebsite.blog.views.feed),
-	(r'^articles/comments/feed$', pdawebsite.blog.views.comment_feed),
+	(r'^feed/?$', pdawebsite.blog.feedviews.feed), # legacy wordpress url
+	(r'^feed/atom/?$', pdawebsite.blog.feedviews.feed), # legacy wordpress url
+	(r'^articles/feed/?$', pdawebsite.blog.feedviews.feed),
+	(r'^articles/comments/feed$', pdawebsite.blog.feedviews.comment_feed),
 	(r'^articles/\d+/\d+/\d+/(?P<slug>[\w-]+)$', pdawebsite.blog.views.article),
 	(r'^articles/\d+/\d+/\d+/(?P<slug>[\w-]+)/comment$', pdawebsite.blog.views.comment),
 )
